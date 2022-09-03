70be3dc11ae469eb230dadbbd1c942e2f0a8e3ac
kkoziel <kkoziel@outlook.com>
2022-09-03 07:58:12 +0200

    refractor: clean mess in code - plots module #2

--- src/plots/plotCanvas.py
+++ src/plots/plotCanvas.py
@@ -4,6 +4,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from abc import abstractmethod
 
+
 class PlotCanvas(FigureCanvas):
 
     def __init__(self, parent=None, width=5, height=3, dpi=100):
